6c6f2f8b037ec6254ac97f86101f50c642c837f0
RajaniMondal <102131572+RajaniMondal@users.noreply.github.com>
2025-08-03 12:15:15 +0530

    Add files via upload

---
+++ Task_1.py
@@ -0,0 +1,13 @@
+a = int(input("Enter the first number: "))
+b= int(input("Enter the Second Number: "))
+
+Addition=(a+b)
+Subtraction =(a-b)
+Multiplication=(a*b)
+Division=(a/b)
+
+print(Addition)
+print(Subtraction)
+print(Multiplication)
+print(Division)
+
---
+++ Task_2.py
@@ -0,0 +1,6 @@
+Firstname = str(input("Enter your first name: "))
+Lastname =str(input("Enter your last name: "))
+fullname = Firstname +" "+ Lastname
+print("Hello," + fullname + "!welcome to the python program")
+
+
